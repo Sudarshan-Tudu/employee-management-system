6265d65a7edc3a1115271738c096bcf41f3ecb27
Sudarshan-Tudu <sudarshantudu97@gmail.com>
2025-02-05 08:10:58 +0530

    first commit

---
+++ Employee_Management_System/src/main/java/com/sudarshan/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.sudarshan.cruddemo.dao;
+
+import com.sudarshan.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+//@RepositoryRestResource(path = "members")
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // No need to write any Code
+}
---
+++ Employee_Management_System/src/main/java/com/sudarshan/cruddemo/entity/Employee.java
@@ -0,0 +1,81 @@
+package com.sudarshan.cruddemo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+    // Create data fields
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    // Create Constructors & Getters/Setters
+
+    public Employee(){
+
+    }
+
+    public Employee(String email, String lastName, String firstName) {
+        this.email = email;
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // defines toString Method
+
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
